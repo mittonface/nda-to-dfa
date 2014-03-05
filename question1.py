15539de8ae541b9d4a42dbc5d66495594006db13
Brent Mitton <brent.mitton@gmail.com>
2014-03-05 18:17:17 -0400

    have the Q_d set now.

--- question1.py
+++ question1.py
@@ -16,10 +16,23 @@ def main():
     state[7] = [("a",Set([6])), ("b",Set([2]))]
     state[8] = []
 
-    new_thing = []
-    for i in range(9):
-        e_closure = e_close(state, num, [])
-        
+
+    # step 1 is to find the states of Q_d, which I'll do by e-closing each of the
+    # states of the nda
+    Q_d_list = []
+
+    for i in range(0,9):
+        # sets are mutable, but after this point I dont intend on changing them
+        # so I'll use frozen set which lets me turn this into a set of sets a 
+        # bit later on
+        state_set  = frozenset(e_close(state, i ,[]))
+        Q_d_list.append(state_set)
+
+
+    # I now have a list of e-closed sets, there could be some duplicates though
+    # so I can turn this into a set of sets
+    Q_d_set = set(Q_d_list)
+    print Q_d_set
 
 # do a little bit of recursing to figure out the epsilon closure for the given
 # number. This should be fine for most problems, but python has a little bit
